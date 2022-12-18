65223b62ff122e834e658071e9d18cb488b6ecd5
venkatesh.sivanesan <venkatesh.sivanesan@zohocorp.com>
2022-12-18 22:43:15 +0530

    js function notes updated - ways of creating function

---
+++ javascript_variables/variables-node.js
@@ -0,0 +1,13 @@
+console.log("JavaScript Variables");
+var global_var = 10;
+function fun_var(){
+    var local_var = 13;
+    console.log("this is global variable ",global_var);
+}
+try{
+    console.log("This is Local Variable ",local_var);
+}catch(err){
+    console.warn("Local variable not found ",err.message);
+}
+
+fun_var();
---
+++ javascript_variables/variables.js
@@ -0,0 +1,3 @@
+console.log("JavaScript Variables Understanding")
+var contents = document.getElementById("div1");
+contents.innerHTML = " JS variables are placeholders for storing values. JS variables are dynamically typed.";
